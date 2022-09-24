0f6dd75110b6bfdc0936d88e2faaa08f6a933d97
safwansadi <safwaniftekher@gmail.com>
2022-09-24 17:52:39 +0600

    Add booking

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 const server = app.listen(port, () =>
   winston.info(`Listening on port ${port}...`)
 );
--- startup/logging.js
+++ startup/logging.js
@@ -1,18 +1,19 @@
-const winston = require('winston');
+const winston = require("winston");
 // require('winston-mongodb');
-require('express-async-errors');
+require("express-async-errors");
 
-module.exports = function() {
-  winston.handleExceptions(
-    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
-  
-  process.on('unhandledRejection', (ex) => {
+module.exports = function () {
+  winston.exceptions.handle(
+    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+  );
+
+  process.on("unhandledRejection", (ex) => {
     throw ex;
   });
-  
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
-  // winston.add(winston.transports.MongoDB, { 
+
+  winston.add(winston.transports.File, { filename: "logfile.log" });
+  // winston.add(winston.transports.MongoDB, {
   //   db: 'mongodb://localhost/vidly',
   //   level: 'info'
-  // });  
-}
+  // });
+};
